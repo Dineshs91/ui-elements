fbe0e21a107ee9a1ddb3d6aeab5f033e230d00ec
dinesh <dineshpy07@gmail.com>
2021-07-14 17:30:50 +0530

    Next iteration of landing page

--- src/elements/home.js
+++ src/elements/home.js
@@ -7,13 +7,13 @@ export default function Home() {
     <div className="flex flex-col h-full">
       <div className="bg-smokey-blue-mesh absolute h-4/5 opacity-70 w-full bg-100%"></div>
       <div className="flex-grow relative text-center z-10">
-        <h1 className="text-3xl pt-4 tracking-wide font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-50 via-blue-100 to-purple-50">UI ELEMENTS</h1>
+        <h1 className="text-3xl pt-4 tracking-wide font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-900 via-gray-50 to-purple-900">UI ELEMENTS</h1>
         <div className="px-12 mx-8 w-full flex">
           <div className="w-1/2 mt-24 h-96 bg-light-smoke bg-opacity-20 bg-cover shadow-2xl rounded-xl">
             <img className="h-full" src={uiImg} />
           </div>
           <div className="mt-40 w-1/2 text-center text-purple-50 font-bold tracking-wide leading-tight text-5xl">
-            <span className="font-hero-primary">Visual Inspiration</span><br/> 
+            <span className="font-hero-primary text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-red-200 to-yellow-100">Visual Inspiration</span><br/> 
             <span className="font-hero-secondary text-3xl">for your next Project</span>
           </div>
         </div>

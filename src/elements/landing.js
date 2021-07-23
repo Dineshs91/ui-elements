9945e953e2cbfeb0ef854ee5ca9333bf57a8e4cf
dinesh <dineshpy07@gmail.com>
2021-07-23 23:09:15 +0530

    Update landing page

--- src/elements/landing.js
+++ src/elements/landing.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { ColorSwatchIcon } from '@heroicons/react/solid';
+import img from '../images/static-website.svg';
+import multimediaImg from '../images/multimedia.svg';
 
 export default function Home() {
   const headingWords = [
@@ -25,23 +27,28 @@ export default function Home() {
   })
 
   return (
-    <div className="mt-4">
-      <div className="flex mx-4 text-purple-700">
+    <div className="mt-4 md:max-w-xl lg:max-w-3xl xl:max-w-5xl md:mx-auto">
+      <div className="flex mx-4 md:mx-0 text-purple-700">
         <ColorSwatchIcon className="h-6 w-6 text-left"></ColorSwatchIcon>
         <p className="ml-2 text-xl font-semibold text-gray-700">UI Elements</p>
       </div>
-      <div className="flex flex-col items-center justify-center">
-        <section className="mt-16">
-          <h1 className="font-bold text-3xl text-gray-900">Design inspiration <br/> for your <br/> <span className="text-purple-600 transition delay-100">{headingWords[headingWordIndex]}</span></h1>
-          <h2 className="mt-8 text-base text-left text-gray-700">
-            A gallery of all the popular components <br/> to help you with your next endeavour.
-          </h2>
-          <button className="cursor-pointer mt-10 p-2 font-bold tracking-wider rounded-lg bg-purple-600 shadow-md hover:bg-purple-700 transition delay-100 text-white w-full">
-            <a href="/home">Browse</a>
-          </button>
+      <div className="flex flex-col md:flex-row items-center justify-center">
+        <section className="flex mt-16 md:w-1/2">
+          <div>
+            <h1 className="font-bold text-3xl lg:text-4xl xl:text-5xl text-gray-900">Design inspiration <br/> for your <br/> <span className="text-purple-600 transition delay-100">{headingWords[headingWordIndex]}</span></h1>
+            <h2 className="mt-8 text-base xl:text-lg text-left text-gray-700">
+              A gallery of all the popular components <br/> to help you with your next endeavour.
+            </h2>
+            <button className="cursor-pointer mt-10 p-2 font-bold tracking-wider rounded-lg bg-purple-600 shadow-md hover:bg-purple-700 transition delay-100 text-white w-full">
+              <a href="/home">Browse</a>
+            </button>
+          </div>
         </section>
-
-        <section className="mt-16 px-4 py-8 bg-gray-50">
+        <img className="mt-8 w-full md:mt-0 md:w-1/2" src={img}></img>
+      </div>
+      <div className="flex flex-col-reverse md:mt-12 md:flex-row items-center justify-center">
+        <img className="w-1/2" src={multimediaImg}></img>
+        <section className="mt-16 md:w-1/2 px-4 bg-gray-50 md:bg-white">
           There are times where you just wanted to glance at existing designs to bake your own ideas.
         </section>
       </div>

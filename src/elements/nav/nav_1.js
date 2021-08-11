54c600b5846acb11b0fa5b1eb50907837b5b033e
dinesh <dineshpy07@gmail.com>
2021-08-11 15:50:54 +0530

    Move Nav1 to its own file

---
+++ src/elements/nav/nav_1.js
@@ -0,0 +1,131 @@
+import { CashIcon, MenuIcon, CogIcon, LogoutIcon, XIcon } from '@heroicons/react/outline';
+import avatarImg from '../../images/navbar/avatar.svg';
+import { Menu, Transition, Popover } from '@headlessui/react';
+import { Fragment, useState } from 'react';
+
+function ProfileDropdown() {
+  return (
+    <Menu as="div">
+      <Menu.Button className="focus:outline-none"><img className="h-7 w-7 mt-1 cursor-pointer rounded-full" src={avatarImg} /></Menu.Button>
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="z-10 py-3 mr-3 md:mr-5 w-28 lg:w-40 xl:w-48 absolute right-0 text-left font-light text-sm lg:text-lg bg-yellow-500 text-gray-900 rounded-md shadow-md focus:outline-none">
+          <Menu.Item>
+            {({ active }) => (
+              <button
+                className={`${active && 'bg-yellow-400 text-black'} pl-3 py-1 w-full`}
+                href="#"
+              >
+                <div className="flex items-center">
+                  <CogIcon className="h-4 w-4 lg:h-5 lg:w-5 xl:h-6 xl:w-6  block"></CogIcon>
+                  <div className="pl-1">Settings</div>
+                </div>
+              </button>
+            )}
+          </Menu.Item>
+          <Menu.Item>
+            {({ active }) => (
+              <button
+              className={`${active && 'bg-yellow-400 text-black'} pl-3 py-1 w-full`}
+                href="#"
+              >
+                <div className="flex items-center">
+                  <LogoutIcon className="h-4 w-4 lg:h-5 lg:w-5 xl:h-6 xl:w-6 block"></LogoutIcon>
+                  <div className="pl-1">Logout</div>
+                </div>
+              </button>
+            )}
+          </Menu.Item>
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  )
+}
+
+function MobileMenu(props) {
+  return (
+    <div className={"absolute left-0 top-10 w-full rounded-b-sm px-4 pt-2 pb-4 bg-gray-800 text-gray-300 " + (!props.isMenuActive && "hidden")}>
+      <div className="space-y-2">
+        <a className="block hover:text-gray-50" href="#">Dashboard</a>
+        <a className="block hover:text-gray-50" href="#">Trade</a>
+      </div>
+    </div>
+  )
+}
+
+function TradePopover() {
+  let [referenceElement, setReferenceElement] = useState()
+  let [popperElement, setPopperElement] = useState()
+
+  return (
+    <Popover className="relative">
+      <Popover.Button ref={setReferenceElement} className="hover:text-gray-50">Trade</Popover.Button>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-200"
+        enterFrom="opacity-0 translate-y-1"
+        enterTo="opacity-100 translate-y-0"
+        leave="transition ease-in duration-150"
+        leaveFrom="opacity-100 translate-y-0"
+        leaveTo="opacity-0 translate-y-1"
+      >
+        <Popover.Panel 
+          ref={setPopperElement}
+          className="absolute top-10 z-10 bg-gray-200 w-72 rounded-md"
+        >
+          <div className="flex flex-col py-4 space-y-4 text-black">
+            <a href="#" className="px-4 hover:bg-gray-100">
+              <div>
+                <p className="text-base xl:text-xl">Buy</p>
+                <p className="text-xs text-gray-600">Buy Cryptcoin at market rates</p>
+              </div>
+            </a>
+            <a href="#" className="px-4 hover:bg-gray-100">
+              <p className="text-base xl:text-xl">Sell</p>
+              <p className="text-xs text-gray-600">Sell your Cryptcoins at the best price</p>
+            </a>
+            <a href="#" className="px-4 hover:bg-gray-100">
+              <p className="text-base xl:text-xl">History</p>
+              <p className="text-xs text-gray-600">History of your past transactions</p>
+            </a>
+          </div>
+        </Popover.Panel>
+      </Transition>
+    </Popover>
+  )
+}
+
+export default function Nav1() {
+  const [isMenuActive, setIsMenuActive] = useState(false);
+
+  return (
+    <div className="mt-8 px-4 flex relative items-center bg-gray-800">
+      <div className="h-5 w-5 md:hidden text-gray-300 cursor-pointer">
+        <MenuIcon onClick={() => setIsMenuActive(!isMenuActive)} className={"" + (isMenuActive && "hidden")}></MenuIcon>
+        <XIcon onClick={() => setIsMenuActive(!isMenuActive)} className={"" + (!isMenuActive && "hidden")}></XIcon>
+      </div>
+      <div className="pl-2 flex flex-grow md:flex-grow-0">
+        <CashIcon className="h-6 w-6 text-yellow-500"></CashIcon>
+        <h2 className="pl-2 font-bold tracking-wide text-yellow-500">Cryptcoin</h2>
+      </div>
+
+      <div className="hidden md:flex flex-grow text-left px-10 space-x-4 text-gray-300">
+        <a className="hover:text-gray-50" href="#">Dashboard</a>
+        <TradePopover></TradePopover>
+      </div>
+
+      <div className="h-10 flex items-center">
+        <ProfileDropdown></ProfileDropdown>
+      </div>
+      <MobileMenu isMenuActive={isMenuActive}></MobileMenu>
+    </div>
+  )
+}
